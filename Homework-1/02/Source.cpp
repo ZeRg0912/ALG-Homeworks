5af01ebe3da9123c2dfa2adf6e234803c0557672
ZeRg0912 <serg.021@yandex.ru>
2023-12-06 16:45:18 +0300

    ---

---
+++ Homework-1/02/Source.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int countRefs(int a[], int size, int point)
+{
+    int count;
+    int left = 0;
+    int right = size - 1;
+    int middle;
+    while (right > left) {
+        middle = (left + right) / 2;
+        if (a[middle] > point) {
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+        cout << middle << endl;;
+    }
+    return count = size - middle;
+}
+
+int enterPoint() {
+    int point;
+    do {
+        cout << "¬ведите точку отсчета: ";
+        cin >> point;
+        system("cls");
+        if (cin.fail()) {
+            cout << "¬ведите число!\n";
+            cin.clear();
+            cin.ignore(256, '\n');
+        }
+        else { break; }
+    } while (!isdigit(point));
+
+    return point;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("chsp1251");
+    system("cls");
+	const int size = 9;
+	int arr[size]{ 14, 16, 19, 32, 32, 32, 56, 69, 72 };
+    int point = enterPoint();
+    cout << countRefs(arr, size, point);
+
+
+	return 0;
+}
